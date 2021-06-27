1846bbf3cc387b10b6f5fbe742f6cf78aa49d795
reazul7 <islamreazul7777@gmail.com>
2021-06-28 04:59:17 +0600

    secure blog post

--- index.js
+++ index.js
@@ -1,82 +1,105 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ObjectID } = require('mongodb');
+const express = require("express");
+const fetch = require("node-fetch");
+const cors = require("cors");
+const { MongoClient, ObjectID } = require("mongodb");
 
-const app = express()
+const app = express();
 
-require('dotenv').config()
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 // console.log(process.env.DB_USER,process.env.DB_PASS,process.env.DB_NAME)
 
-
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5ae7d.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
+const isAdmin = (email) => {
+  return fetch("http://localhost:5000/isAdmin", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email }),
+  })
+    .then((response) => response.json())
+    .then((data) => data)
+    .catch((err) => {
+      console.log("is admin check func err", err);
+    });
+};
 
-client.connect(err => {
+client.connect((err) => {
   const adminCollection = client.db(process.env.DB_NAME).collection("admin");
   const blogsCollection = client.db(process.env.DB_NAME).collection("blogs");
   // perform actions on the collection object
-  console.log('Database connection established')
+  console.log("Database connection established");
   // client.close();
 
   // make admin
-  app.post('/makeAdmin', (req, res) => {
+  app.post("/makeAdmin", (req, res) => {
     const user = req.body;
-    adminCollection.insertOne(user)
-    .then(result => {
-      console.log('inserted count', result.insertedCount)
-      res.send(result.insertedCount > 0)
-    })
-  })
+    adminCollection.insertOne(user).then((result) => {
+      console.log("inserted count", result.insertedCount);
+      res.send(result.insertedCount > 0);
+    });
+  });
 
   // add blogs
-  app.post('/addBlog', (req, res) => {
+  app.post("/addBlog", (req, res) => {
     const newBlog = req.body;
-    console.log(newBlog)
-    blogsCollection.insertOne(newBlog)
-    .then(result => {
-      console.log('inserted count', result.insertedCount)
-      res.send(result.insertedCount > 0)
-    })
-  })
+    console.log("check add blog body data", req.body);
+    isAdmin(req.body.email)
+      .then((res) => {
+        console.log("is admin or not", res);
+        if (res) {
+          blogsCollection.insertOne(newBlog).then((result) => {
+            console.log("inserted count", result.insertedCount);
+            res.send(result.insertedCount > 0);
+          });
+        } else {
+          res.send({ status: 401, message: "Unauthorized" });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
 
   // get blogs
-  app.get('/blogs', (req, res) => {
-    blogsCollection.find({})
-    .toArray((err, items) => {
-      res.send(items)
-    })
-  })
+  app.get("/blogs", (req, res) => {
+    blogsCollection.find({}).toArray((err, items) => {
+      res.send(items);
+    });
+  });
 
   // admin check
-  app.post('/isAdmin', (req, res) => {
+  app.post("/isAdmin", (req, res) => {
     const email = req.body.email;
-    adminCollection.find({email: email})
-    .toArray((err, admins) => {
-      res.send(admins.length > 0)
-    })
-  })
+    console.log("is admin api ", email);
+    adminCollection.find({ email: email }).toArray((err, admins) => {
+      res.send(admins.length > 0);
+    });
+  });
 
   // single blog post
-  app.get('/blogDetails/:id', (req, res) => {
-    blogsCollection.find({_id:ObjectID(req.params.id)})
-    .toArray((err, documents) => {
-      res.send(documents[0])
-      console.log(err,documents)
-    })
-  })
+  app.get("/blogDetails/:id", (req, res) => {
+    blogsCollection
+      .find({ _id: ObjectID(req.params.id) })
+      .toArray((err, documents) => {
+        res.send(documents[0]);
+        console.log(err, documents);
+      });
+  });
 });
 
-
-app.listen(port) 
+app.listen(port);
