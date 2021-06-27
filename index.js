1cb326f59eec9066d548c7f108f552078695cf94
reazul7 <islamreazul7777@gmail.com>
2021-06-28 03:07:09 +0600

    hurre fix api not found  error

--- index.js
+++ index.js
@@ -21,15 +21,17 @@ app.get('/', (req, res) => {
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5ae7d.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
 client.connect(err => {
-  const adminCollection = client.db("retro-blog").collection("admin");
-  const blogsCollection = client.db("retro-blog").collection("blogs");
+  const adminCollection = client.db(process.env.DB_NAME).collection("admin");
+  const blogsCollection = client.db(process.env.DB_NAME).collection("blogs");
   // perform actions on the collection object
   console.log('Database connection established')
   // client.close();
 
   // make admin
-  app.post('makeAdmin', (req, res) => {
+  app.post('/makeAdmin', (req, res) => {
     const user = req.body;
     adminCollection.insertOne(user)
     .then(result => {
@@ -39,8 +41,9 @@ client.connect(err => {
   })
 
   // add blogs
-  app.post('addBlog', (req, res) => {
+  app.post('/addBlog', (req, res) => {
     const newBlog = req.body;
+    console.log(newBlog)
     blogsCollection.insertOne(newBlog)
     .then(result => {
       console.log('inserted count', result.insertedCount)
@@ -57,7 +60,7 @@ client.connect(err => {
   })
 
   // admin check
-  app.post('isAdmin', (req, res) => {
+  app.post('/isAdmin', (req, res) => {
     const email = req.body.email;
     adminCollection.find({email: email})
     .toArray((err, admins) => {
