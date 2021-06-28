4fb3af692ffc063783d2b14e6f763300eb3a377b
reazul7 <islamreazul7777@gmail.com>
2021-06-28 06:02:12 +0600

    add server remote url

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 });
 
 const isAdmin = (email) => {
-  return fetch("http://localhost:5000/isAdmin", {
+  return fetch("https://desolate-savannah-78335.herokuapp.com/isAdmin", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
